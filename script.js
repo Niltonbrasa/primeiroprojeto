24b758f8b2e6d94c919ab9fe10b13c57f4125d0c
Nilton Ribeiro <niltonrihash@gmail.com>
2023-09-07 18:55:26 -0300

    :tata: initial comit

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+
+    // pegar a tag img
+    //const img = document.querySelector("#profiler img")
+
+    // subistituir a imagem
+    //if(html.classList.contains('light')){
+  
+    //se tiver light mode, adicionar a imagem light
+   // img.setAttribute('src', './assets/avatarb.png')
+  //  } else {
+      // se tiver sem light mode, manter a image normal
+  //    img.setAttribute('src', '.assets/avatar.png')
+ //  }
+  }
+
+ 
+
+
